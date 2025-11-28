ef9c80bab606a6b8155f2d9374673cbac62d4250
Sirine <dataengineering90s@gmail.com>
2025-11-28 10:38:12 +0100

    second commit ETL

--- src/etl.py
+++ src/etl.py
@@ -5,9 +5,9 @@ Created on Fri Nov 28 09:32:38 2025
 @author: sirin
 """
 
-from src.extract import extract_data
-from src.transform import clean_data
-from src.load import load_to_db
+from extract import extract_data
+from transform import clean_data
+from load import load_to_db
 
 def run_etl():
     df = extract_data("data/sales.csv")
